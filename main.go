418ee9b0f521c6615ff85ce98f7a0f0273055b2b
Stanislav Grokholsky <stangrokh@gmail.com>
2017-08-19 23:10:37 +0300

    pool added, reference reworked

--- main.go
+++ main.go
@@ -1,57 +1,63 @@
 package main
 
 import (
-	"os"
+	"bufio"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"./workers"
 )
 
-func contains(s []string, e string) bool {
-	for _, a := range s {
+func contains(s []string, e string) (bool, int) {
+	for i, a := range s {
 		if a == e {
-			return true
+			return true, i
 		}
 	}
-	return false
+	return false, -1
 }
 
-func checkWordInLists(check map[rune]map[string]bool, first rune, word []rune) bool {
+func checkWordInLists(check map[rune][]string, first rune, word string) bool {
 
-	alias := map[rune]string {
+	alias := map[rune]string{
 		'-': "exclude words",
 		'!': "essential words",
 		'#': "keywords",
 	}
 
-	for _, valmap := range check {
-		_, ok := valmap[string(word)]
+	for key, valmap := range check {
+		ok, _ := contains(valmap, word)
 		if ok {
-			log.Printf("%s: Word is in %s already.\n", string(word), alias[first])
+			log.Printf("%s: Word is in %s already.\n", string(word), alias[key])
 			return true
 		}
 	}
 	return false
 }
 
-func parseWord(str string) (first rune, word []rune) {
+func parseWord(str string) (first rune, word string) {
 	runes := []rune(str)
 	first = runes[0]
 	// Смотрим на первый символ ключевого слова
 	switch first {
 	case '-':
-		word = runes[1:]
+		word = string(runes[1:])
 	case '!':
-		word = runes[1:]
+		word = string(runes[1:])
 	default:
 		// Обычное ключевое слово, обозначим через '#'
 		first = '#'
-		word = runes[:]
+		word = string(runes[:])
 	}
 
 	return first, word
 }
 
-func addWords(reference map[rune]map[string]bool, words []string) {
+func addWords(reference map[rune][]string, words []string) {
 
 	for _, r := range words {
 
@@ -63,15 +69,15 @@ func addWords(reference map[rune]map[string]bool, words []string) {
 		}
 		_, ok := reference[first]
 		if !ok {
-			reference[first] = make(map[string]bool)
+			reference[first] = []string{}
 		}
-		reference[first][string(word)] = true
+		reference[first] = append(reference[first], word)
 	}
 }
 
-func delWords(reference map[rune]map[string]bool, words []string) {
+func delWords(reference map[rune][]string, words []string) {
 
-	alias := map[rune]string {
+	alias := map[rune]string{
 		'-': "exclude words",
 		'!': "essential words",
 		'#': "keywords",
@@ -80,11 +86,16 @@ func delWords(reference map[rune]map[string]bool, words []string) {
 	for _, r := range words {
 
 		first, word := parseWord(r)
-		_, ok := reference[first][string(word)]
-		if !ok {
-			log.Fatalf("Keyword error:\n%s: No such word in %s\n", string(word), alias[first])
+		// _, ok := reference[first][string(word)]
+		// if !ok {
+		// 	log.Fatalf("Keyword error:\n%s: No such word in %s\n", string(word), alias[first])
+		// }
+		ok, i := contains(reference[first], word)
+		if ok {
+			reference[first] = append(reference[first][:i], reference[first][i+1:]...)
+		} else {
+			log.Printf("Keyword error:\n%s: No such word in %s\n", string(word), alias[first])
 		}
-		delete(reference[first], string(word))
 	}
 }
 
@@ -95,70 +106,91 @@ func getNewsSubjects(keywords []string) []string { //сделать структ
 
 func main() {
 
-	commandsList := []string {
+	commandsList := []string{
 		"start",
-		"end",
+		"stop",
 		"topic",
 		"add",
 		"del",
 		"sort",
 	}
 
-	argsLenDict := map[string]int {
+	argsLenDict := map[string]int{
 		"start": 0,
-		"end": 0,
+		"stop":  0,
 		"topic": 1,
-		"add": -1,
-		"del": -1,
-		"sort": 1,
+		"add":   -1,
+		"del":   -1,
+		"sort":  1,
 	}
 
-	const maxArgs = 10
+	// const maxArgs = 10
+	//
+	// if len(str) < 1 {
+	// 	fmt.Printf("help")
+	// 	return
+	// }
 
-	if len(os.Args) < 1 {
-		fmt.Printf("help")
-		return
-	}
+	pool := workers.NewPool(5)
 
-	var command string = os.Args[1]
+	reference := make(map[rune][]string)
 
-	if !contains(commandsList, command) {
-		panic("Wrong command!") // Заменить панику на что-то более спокойное.
-	}
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
 
-	//if len(os.Args) > 15 {
-	//	panic("Too much arguments")
-	//}
+		str := strings.Split(scanner.Text(), " ")
+		var command = str[0]
+		ok, _ := contains(commandsList, command)
+		if !ok {
+			panic("Wrong command!") // Заменить панику на что-то более спокойное.
+		}
 
-	if(argsLenDict[command] >= 0) {
-		if (len(os.Args) != argsLenDict[command] + 1) {
-			panic("Wrong number of arguments")
+		//if len(str) > 15 {
+		//	panic("Too much arguments")
+		//}
+
+		if argsLenDict[command] >= 0 {
+			if len(str) != argsLenDict[command]+1 {
+				panic("Wrong number of arguments")
+			}
+		}
+
+		/*	for i, r := range str[2:] {
+				parse_arg(command, r, i)
+			}
+		*/
+		switch command {
+		case "start":
+			res, err := http.Get("http://otvet.mail.ru/search/" + strings.Join(reference['#'], " "))
+			if err != nil {
+				log.Fatal(err)
+			}
+			robots, err := ioutil.ReadAll(res.Body)
+			res.Body.Close()
+
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("%s", robots)
+			// pool.Run()
+		case "stop":
+			pool.Stop()
+		case "topic":
+			//setTopic(str[2])
+		case "add":
+			addWords(reference, str[1:])
+			break
+		case "del":
+			delWords(reference, str[1:])
+			break
+		case "sort":
+			break
 		}
 	}
-
-	reference := map[rune]map[string]bool{}
-	reference = make(map[rune]map[string]bool)
-/*	for i, r := range os.Args[2:] {
-		parse_arg(command, r, i)
-	}
-*/
-	switch command {
-	case "topic":
-		//setTopic(os.Args[2])
-	case "add":
-		addWords(reference, os.Args[2:])
-		break
-	case "del":
-		delWords(reference, os.Args[2:])
-		break
-	case "sort":
-		break
-	}
-
-	for first, valmap := range reference {
+	for first, words := range reference {
 		fmt.Printf("list %q:\n", first)
-		for key, _ := range valmap {
-			fmt.Printf("%s, ", key)
+		for _, word := range words {
+			fmt.Printf("%s, ", word)
 		}
 		fmt.Printf("\n\n")
 	}
